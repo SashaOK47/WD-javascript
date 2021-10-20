2ae4f3ba32a7e4cc535b4fd179077484c9a21199
Sasha Okoronko <hvorost22@mail.ru>
2021-10-20 23:34:46 +0300

    JS-2: - fixed bugs calculate

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,42 +1,54 @@
-const calcResult = document.querySelector('.calc__result');
-const inputBtns = document.querySelectorAll('.calc__button');
-const cleanBtn = document.querySelector('.calc__button--clean');
-const btnOn = document.querySelector('.calc__button--on');
-const cleanOne = document.querySelector('.calc__button--clean-one');
+const calcResult = document.querySelector(".calc__result");
+const inputBtns = document.querySelectorAll(".calc__button");
+const cleanBtn = document.querySelector(".calc__button--clean");
+const btnOn = document.querySelector(".calc__button--on");
+const cleanOne = document.querySelector(".calc__button--clean-one");
 
-
+let result = false;
 
 const clear = () => {
-  calcResult.value = '';
+  calcResult.value = "";
 };
 
-btnOn.addEventListener('click', () => {
-  calcResult.classList.toggle('calc-on');
+btnOn.addEventListener("click", () => {
+  calcResult.classList.toggle("calc-on");
   clear();
-})
+});
 
 const sendNumberValue = (num) => {
-  if(num == '.') {
-      if(calcResult.value == '') {
-        calcResult.value += 0;
-      }
-      if(calcResult.value.includes('.')) {
-          num = '';
-      }
+  if (result) {
+    calcResult.value = "";
+    result = false;
+  }
+
+  if (num == ".") {
+    if (calcResult.value == "") {
+      calcResult.value += 0;
+    }
+    if (calcResult.value.includes(".")) {
+      num = "";
+    }
+  } else if (calcResult.value.indexOf(".") == -1) {
+    calcResult.value = calcResult.value.replace("0", "");
   }
   calcResult.value += num;
-}
+};
 
 const useOperator = (op) => {
-  if(calcResult.value) {
+  result = false;
+  if (op == "-") {
+    calcResult.value += op;
+    calcResult.value = calcResult.value.replace("--", "-");
+  } else if (calcResult.value) {
     calcResult.value += op;
   }
-}
+};
 
 const eql = () => {
-  if(calcResult.value == '') return;
+  if (calcResult.value == "") return;
   calcResult.value = Math.round(eval(calcResult.value) * 100000000) / 100000000;
-}
+  result = true;
+};
 
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.contains("num")) {
@@ -48,10 +60,20 @@ inputBtns.forEach((inputBtn) => {
   }
 });
 
-cleanOne.addEventListener('click', () => {
+cleanOne.addEventListener("click", () => {
   let exp = calcResult.value;
   calcResult.value = exp.substring(0, exp.length - 1);
 });
 
-
 cleanBtn.addEventListener("click", clear);
+
+window.addEventListener("keydown", (e) => {
+  if ((e.key >= 0 && e.key <= 9) || e.key == ".") {
+    sendNumberValue(e.key);
+  } else if (e.key == "+" || e.key == "-" || e.key == "/" || e.key == "*") {
+    useOperator(e.key);
+  } else if (e.key == "=" || e.key == "Enter") {
+    e.preventDefault();
+    eql();
+  }
+});
